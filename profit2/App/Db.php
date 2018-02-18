4c202802f15699a09edcf7e857697b47695d875c
Limon1980 <tarkalmob@yandex.ru>
2018-02-18 18:19:42 +1000

    + метод execute класса Db

--- profit2/App/Db.php
+++ profit2/App/Db.php
@@ -4,8 +4,17 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
+
     public function __construct()
     {
-        echo 'Hello, DB!';
+        $this->dbh = new \PDO('mysql:host=localhost; dbname=test', 'taras', 'TRargo.12');
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
     }
 }
